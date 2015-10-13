33457a4b0de30018c3664cca16fe78fd4feada9a
doppl3r <deben3@gmail.com>
2015-10-13 12:12:15 -0700

    updated url readability

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,16 @@ var $resize = $(window).resize(function () { if (!isMobile()) fullscreen(null); 
 
 function isMobile() {return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)); }
 
+$( window ).load(function() {
+    sliceExtension();
+});
+
+function sliceExtension(){
+    //remove fragment as much as it can go without adding an entry in browser history:
+    window.location.replace("#");
+    // slice off the remaining '#' in HTML5:    
+    if (typeof window.history.replaceState == 'function') {
+        var href=window.location.href;
+        history.replaceState({}, '', window.location.href.slice(0, href.lastIndexOf(".")));
+    }
+}
